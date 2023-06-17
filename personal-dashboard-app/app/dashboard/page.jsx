d624162fb47f093d68502a624eb12d378f6b3329
John Ramos <jramos5883@gmail.com>
2023-06-16 18:04:50 -0700

    Ready to start building out the dashboard features.

--- personal-dashboard-app/app/dashboard/page.jsx
+++ personal-dashboard-app/app/dashboard/page.jsx
@@ -1,3 +1,17 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function DefaultDashboard() {
-  return <h1>Default Dashboard</h1>;
+  const session = useSession();
+  if (!session) {
+    redirect("/landingpage");
+  }
+
+  return (
+    <h1 className="text-3xl text-center p-4">
+      You must be logged in to see your dashboard, please sign back in to
+      continue.
+    </h1>
+  );
 }
